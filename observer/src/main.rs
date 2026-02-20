751ac2cc0f312fc228c58fd59b7f6597a40b4d79
N10h0ggr <noel.carrascoit@gmail.com>
2026-02-20 06:02:08 -0800

    fix observer deps

--- observer/src/main.rs
+++ observer/src/main.rs
@@ -1,10 +1,9 @@
+use log;
 use std::ffi::c_void;
 use std::ptr::{null, null_mut};
-use log;
 use windows_sys::Win32::System::Threading::{
-    CreateThread, GetCurrentProcessId, WaitForSingleObject
+    CreateThread, GetCurrentProcessId, WaitForSingleObject,
 };
-
 
 mod etw;
 use etw::filter;
@@ -12,11 +11,11 @@ use etw::utils::guid_to_string;
 
 const MICROSOFT_WINDOWS_KERNEL_AUDIT_API_CALLS: &str = "E02A841C-75A3-4FA7-AFC8-AE09CF9B7F23";
 const MICROSOFT_WINDOWS_THREAT_INTEL: &str = "F4E1897C-BB5D-5668-F1D8-040F4D8DD344";
+
 const EVENTID_OPENTHREAD: u16 = 4;
 const EVENTID_SETTHREADCONTEXT: u16 = 6;
 
-const EVENT_ENABLE_PROPERTY_STACK_TRACE: u32 = 4; 
-
+const EVENT_ENABLE_PROPERTY_STACK_TRACE: u32 = 4;
 
 // This function is passed to a trampoline function that matches the needed C
 // structure and parses the EVENT_RECORD using the provider schema or TDH
@@ -37,7 +36,7 @@ fn syscall_detection_callback(event: &etw::Event) {
             if let Ok(ret) = event.get_property("ReturnCode") {
                 log::debug!("  -> ReturnCode: {}", ret);
             }
-        },
+        }
         // Event 5: OpenProcess - TargetProcessId, DesiredAccess, ReturnCode
         5 => {
             log::info!("[Event 5] OpenProcess Detected from PID: {}", event.pid());
@@ -50,10 +49,13 @@ fn syscall_detection_callback(event: &etw::Event) {
             if let Ok(ret) = event.get_property("ReturnCode") {
                 log::debug!("  -> ReturnCode: {}", ret);
             }
-        },
+        }
         // Event 6: SetThreadContext - TargetProcessId, TargetThreatId, DesiredAccess
         6 => {
-            log::info!("[Event 6] SetThreadContext Detected from PID: {}", event.pid());
+            log::info!(
+                "[Event 6] SetThreadContext Detected from PID: {}",
+                event.pid()
+            );
             if let Ok(target_pid) = event.get_property("TargetProcessId") {
                 log::info!("  -> Target PID: {}", target_pid);
             }
@@ -68,7 +70,7 @@ fn syscall_detection_callback(event: &etw::Event) {
             if let Ok(ret) = event.get_property("ReturnCode") {
                 log::debug!("  -> ReturnCode: {}", ret);
             }
-        },
+        }
         // Handle other IDs generic logging if needed
         id => {
             log::debug!("Event ID {} detected from PID: {}", id, event.pid());
@@ -96,7 +98,6 @@ unsafe extern "system" fn event_loop_entry(user_trace_ptr: *mut c_void) -> u32 {
 
 // TODO: Add proper return type for Windows
 fn main() {
-
     env_logger::init();
 
     // Define the Session (User or Kernel)
@@ -106,13 +107,16 @@ fn main() {
     let mut provider = etw::Provider::new(MICROSOFT_WINDOWS_KERNEL_AUDIT_API_CALLS)
         .trace_flags(EVENT_ENABLE_PROPERTY_STACK_TRACE);
 
-    provider.load_manifest().expect("Failed to load provider manifest");
+    provider
+        .load_manifest()
+        .expect("Failed to load provider manifest");
     if let Some(manifest) = &provider.manifest {
         log::trace!("Provider manifest: {:?}", manifest);
     }
 
     let mut filter_open_thread = etw::EventFilter::new(filter::DoesMatch(EVENTID_OPENTHREAD));
-    let mut filter_set_context_threat = etw::EventFilter::new(filter::DoesMatch(EVENTID_SETTHREADCONTEXT));
+    let mut filter_set_context_threat =
+        etw::EventFilter::new(filter::DoesMatch(EVENTID_SETTHREADCONTEXT));
 
     filter_open_thread.add_callback(syscall_detection_callback);
     filter_set_context_threat.add_callback(syscall_detection_callback);
@@ -120,7 +124,11 @@ fn main() {
     provider.add_filter(filter_open_thread);
     provider.add_filter(filter_set_context_threat);
 
-    log::debug!("Enabling provider {}: {}", provider.name, guid_to_string(&provider.guid));
+    log::debug!(
+        "Enabling provider {}: {}",
+        provider.name,
+        guid_to_string(&provider.guid)
+    );
 
     user_trace.enable(provider).unwrap();
 
@@ -151,8 +159,6 @@ fn main() {
     //     hhandle: HANDLE,
     //     dwmilliseconds: u32,
     // ) -> WAIT_EVENT
-    unsafe { WaitForSingleObject(trace_thread_handle, 10000) };
+    unsafe { WaitForSingleObject(trace_thread_handle, 20000) };
     user_trace.stop();
 }
-
-
